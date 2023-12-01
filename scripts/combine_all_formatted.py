4160184ff3056a70d9c08b938daa230c9fcdacc2
Branden Vongphakdy <vbranden_@hotmail.com>
2023-12-01 16:52:25 -0500

    data update

---
+++ scripts/combine_all_formatted.py
@@ -0,0 +1,37 @@
+import os
+import csv
+import re
+import requests
+from bs4 import BeautifulSoup
+import operator
+import datetime
+
+years = ['2023', '2022', '2021', '2020', '2019']
+
+date = datetime.datetime.now()
+version = date.strftime("%y_%m_%d_%f")
+csv_filename = f"data/master/{version}.csv"
+output_directory = 'data/formatted/'
+os.makedirs(output_directory, exist_ok=True)
+header = ['PlayDate', 'AP'] + [f'N{i:02d}' for i in range(1, 21)]
+master_year_list = []
+
+for year in years:
+    # File paths for the existing CSV files
+    year_filename = os.path.join(output_directory, f'{year}.csv')
+    with open(year_filename, newline='') as csvfile:
+        year_csv = csv.reader(csvfile, quotechar='|')
+        year_data  = list(year_csv)
+        year_data .pop(0)
+        master_year_list.extend(year_data)
+
+length = len(list(master_year_list))
+
+os.makedirs(os.path.dirname(csv_filename), exist_ok=True)
+
+with open(csv_filename, 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(header)
+    for idx in range(length):
+        row_data_year = master_year_list[idx]
+        csv_writer.writerow(row_data_year)
--- scripts/webscraper.py
+++ scripts/webscraper.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 import re
 
 
-type ='pm'
+#type ='am'
 year = '2023'
 
 Time_AM = {
@@ -20,30 +20,35 @@ Time_PM = {
     "code": "P",
     "label": "evening",
 }
+
 Times = {'am' : Time_AM, 'pm' : Time_PM}
 
+time_sets = ['am', 'pm']
 
-URL = f"https://www.lotteryleaf.com/on/daily-keno-{Times[type]['label']}/{year}"
+for t in time_sets:
+    type = t
+    URL = f"https://www.lotteryleaf.com/on/daily-keno-{Times[type]['label']}/{year}"
 
-page = requests.get(URL)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
 
-soup = BeautifulSoup(page.content, "html.parser")
+    winning_numbers = soup.find_all('ul', class_='nbr-grp')
+    dates = soup.find_all('td', class_='win-nbr-date')
 
-winning_numbers = soup.find_all('ul', class_='nbr-grp')
-dates = soup.find_all('td', class_='win-nbr-date')
+    dates.pop(0)  # remove date title
 
-dates.pop(0)  # remove date title
+    if winning_numbers:
+        csv_filename = f"data/scraping/extracted_numbers_{Times[type]['label']}_{year}.csv"
+        header = ['PlayDate', 'AP'] + [f'N{i:02d}' for i in range(1, 21)]
+        with open(csv_filename, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(header)
+            for idx, winning_number in enumerate(winning_numbers):
+                date_element = re.sub(r'[^a-zA-Z0-9 ]', '', dates[idx].text.strip()) if dates[idx] else ""
+                row_data = [date_element] + [Times[type]['code']] + winning_number.text.split()  # Assuming winning_number is a space-separated string
+                # Write the row to the CSV file
+                csv_writer.writerow(row_data)
+        print("Writing to CSV file complete.")
 
-if winning_numbers:
-    csv_filename = f"data/scraping/extracted_numbers_{Times[type]['label']}_{year}.csv"
-    header = ['PlayDate', 'AP'] + [f'N{i:02d}' for i in range(1, 21)]
-    with open(csv_filename, 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(header)
-        for idx, winning_number in enumerate(winning_numbers):
-            date_element = re.sub(r'[^a-zA-Z0-9 ]', '', dates[idx].text.strip()) if dates[idx] else ""
-            row_data = [date_element] + [Times[type]['code']] + winning_number.text.split()  # Assuming winning_number is a space-separated string
-            # Write the row to the CSV file
-            csv_writer.writerow(row_data)
-else:
-    print("No ul element found with class 'nbr-grp'.")
+    else:
+        print("No ul element found with class 'nbr-grp'.")
