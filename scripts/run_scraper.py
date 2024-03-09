6192bc53452acf064613efeed69fa40007089e52
Branden Vongphakdy <vbranden_@hotmail.com>
2024-03-09 18:46:08 -0500

    import 2024-03-09 midday

--- scripts/run_scraper.py
+++ scripts/run_scraper.py
@@ -4,10 +4,14 @@
 
 import subprocess
 
+activate_command = "source ./.venv/Scripts/activate"  # Adjust the path
+subprocess.run(activate_command, shell=True)
+
+
 # Run script1.py
-subprocess.run(["python", "./scripts/scraper/Keno/webscraper.py"])
+subprocess.run(["python", "scripts\scraper\Keno\webscraper.py"])
 
 # Run script2.py
-subprocess.run(["python", "./scripts/scraper/Keno/cleandata.py"])
+subprocess.run(["python", "scripts\scraper\Keno\cleandata.py"])
 
-subprocess.run(["python", "./scripts/scraper/Keno/cleandata.py"])
+subprocess.run(["python", "scripts\scraper\Keno\order_csv.py"])
